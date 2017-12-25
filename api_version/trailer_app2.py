2522aa58be133452b53e132c3ea4cecaf25770d7
Josh <jbreck1987@users.noreply.github.com>
2017-12-24 20:05:33 -0500

    Added comments

--- api_version/trailer_app2.py
+++ api_version/trailer_app2.py
@@ -1,8 +1,11 @@
 from movie_helper2 import Movie
 from fresh_tomatoes2 import open_movies_page
 
+# Used for auth in API calls
 api_key = ''
 
+
+# Assigning movie IDs to more readable variables
 blade_id = 'tt0120611'
 dogma_id = 'tt0120655'
 lebowski_id = 'tt0118715'
@@ -10,8 +13,10 @@ the_matrix_id = 'tt0133093'
 american_pie_id = 'tt0163651'
 inception_id = 'tt1375666'
 
+# Used for the append_to_request feature of the TMDb API
 appends = ('videos',)
 
+# Instantiate and build movie objects
 blade_movie = Movie(blade_id, api_key, append_vals=appends)
 blade_movie.build()
 
@@ -30,7 +35,9 @@ american_pie_movie.build()
 inception_movie = Movie(inception_id, api_key, append_vals=appends)
 inception_movie.build()
 
+# List of movie objects to pass to the webpage building function
 movies = (blade_movie, dogma_movie, lebowski_movie, the_matrix_movie,
           american_pie_movie, inception_movie)
 
+# Create webpage
 open_movies_page(movies)
